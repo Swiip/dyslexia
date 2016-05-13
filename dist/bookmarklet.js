efac4b3858c3e10fbb06ace2298438b2470e48b4
Matthieu Lux <matthieu.lux@gmail.com>
2016-05-13 09:15:14 +0200

    commit dist result

---
+++ dist/bookmarklet.js
@@ -0,0 +1,2 @@
+// Copy this to your URL bar:
+javascript:!function(){function n(e){if(e.children.length>0)for(var l=0;l<e.children.length;l++)n(e.children[l]);else e.textContent=t(e.textContent)}function t(n){for(var t=n.split(" "),l=0;l<t.length;l++)t[l].length>3&&(t[l]=e(t[l]));return t.join(" ")}function e(n){for(var t=n[0],e=n.slice(1,n.length-1).split("");e.length;){var l=Math.floor(Math.random()*e.length),r=e.splice(l,1);t+=r}return t+=n[n.length-1]}window.setInterval(function(){n(document)},500)}();
